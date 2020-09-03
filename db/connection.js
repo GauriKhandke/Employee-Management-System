b497b5ac0b030a0a8ffa50bacb673d483c9725f0
Gauri Khandke <khandkegauri@gmail.com>
2020-09-02 18:53:12 -0700

    Added starter code

---
+++ db/connection.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const util = require('util');
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host : 'localhost',
+
+    //username
+    user : 'root',
+
+    //password
+    password : 'asdf@123',
+
+    // database name to connect with
+    database : 'employee_DB'
+});
+
+connection.connect();
+
+// setting up connection.query to use promises instead of callbacks
+// This allows us to use async/await syntax
+connection.query = util.promisify(connection.query);
+
+module.exports = connection;
