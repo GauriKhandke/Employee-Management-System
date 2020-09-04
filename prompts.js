15ba06c8c80ca363b03637b86903d20831138aae
Gauri Khandke <khandkegauri@gmail.com>
2020-09-04 16:23:13 -0700

    Updated prompt for mainPrompt

--- prompts.js
+++ prompts.js
@@ -6,12 +6,16 @@ module.exports = {
             message: 'What would you like to do?',
             choices :[
                 'View all employees',
+                'View all roles',
+                'View all departments',
                 'View all employees by department',
                 'View all employees by manager',
                 'Add Employee',
-                'Remove Employee',
+                'Add Department',
+                'Add Role',
                 'Update employee role',
                 'Update employee manager',
+                'Remove Employee',
                 'Exit'
             ]
         }
