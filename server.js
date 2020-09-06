63f67c1998115b41d8d8b92b73229daf2ba29aaf
Gauri Khandke <khandkegauri@gmail.com>
2020-09-05 21:06:01 -0700

    Added function for add Role and add Employee

--- server.js
+++ server.js
@@ -47,22 +47,79 @@ async function viewEmployeesByDepartment(){
 	mainPrompt();
 }
 
-async function getDepartments(){
-	
-	const departments = await db.getDepartments();
+async function addEmployee(){
 
-    console.log("\n");
-    console.table(departments);
+	const rolesResult = await db.getRoles();
 	
-	mainPrompt();
+	let roleNames = [];
+	for (let i = 0; i < rolesResult.length; i++) {
+		roleNames.push(rolesResult[i].title)
+	}
+
+	const employeeResult = await db.getEmployees();
+	
+	let employeeNames = [];
+	for (let i = 0; i < employeeResult.length; i++) {
+		employeeNames.push(employeeResult[i].first_name+ " "+ employeeResult[i].last_name)
+	}
+
+	prompts.addEmployee.push({
+		type : 'list',
+		name : 'roleName',
+		message : "What is role ?",
+		choices: roleNames
+	});
+	prompts.addEmployee.push({
+		type : 'list',
+		name : 'managerName',
+		message : "What is manager name ?",
+		choices: employeeNames
+	});
+
+	const {firstName, lastName, roleName, managerName} = await inquirer.prompt(prompts.addEmployee);
+	const managerFirstName = managerName.split(" ")[0];
+	const managerLastName = managerName.split(" ")[1];
+
+	const roleId = rolesResult.filter(role => role.title === roleName)[0].id;
+	const managerId = employeeResult.filter(employee => employee.first_name === managerFirstName && employee.last_name === managerLastName)[0].id;
+
+	const addEmployeeResult = await db.addEmployee(firstName, lastName, roleId, managerId);
+
+	viewAllEmployees();
 }
 
 async function addDepartment(){
+	
 	const { deptName } = await inquirer.prompt(prompts.addDepartment);
 
 	const result = await db.addDepartment(deptName);
 
 	viewAllDepartments();
+}
+
+
+async function addRole(){
+
+	const departmentsResult = await db.getDepartments();
+	
+	let departments = [];
+	for (let i = 0; i < departmentsResult.length ;i++){
+    	departments.push(departmentsResult[i].name);
+    }
+	
+	prompts.addRole.push({
+		type : 'list',
+		name : 'departmentName',
+		message : "What is role's department Name ?",
+		choices: departments
+	});
+
+	const { roleTitle , roleSalary, departmentName} = await inquirer.prompt(prompts.addRole);
+
+	const departmentId = departmentsResult.filter(res => res.name === departmentName)[0].id;
+	const addRoleResult = await db.addRole(roleTitle, roleSalary, departmentId);
+
+	viewAllRoles();
 }
 
 async function mainPrompt() {
@@ -89,6 +146,7 @@ async function mainPrompt() {
 			break;
 
 		case 'Add Employee':
+			addEmployee();
 			break;
 
 		case 'Add Department':
@@ -96,6 +154,7 @@ async function mainPrompt() {
 			break;
 
 		case 'Add Role':
+			addRole();
 			break;
 
 		case "Update employee role":
