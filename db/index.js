cad2a94d1079f5fab2c16b1ba808f937e7d3c9c1
Gauri Khandke <khandkegauri@gmail.com>
2020-09-05 21:04:03 -0700

    Added function for get employees, roles, departments, add Role and add Employee

--- db/index.js
+++ db/index.js
@@ -77,13 +77,91 @@ class DB{
     }
 
     getDepartments(){
-        return this.connection.query(
-            `
-            SELECT 
-	            name AS Departments 
-            FROM department;
-            `
-        );
+        try{
+            return this.connection.query(
+                `
+                SELECT 
+                    * 
+                FROM department;
+                `
+            );
+        }
+        catch(err){
+            if(err) throw err;
+        }   
+    }
+
+    getRoles(){
+        try{
+            return this.connection.query(
+                `
+                SELECT 
+                    * 
+                FROM role;
+                `
+            );
+        }
+        catch(err){
+            if(err) throw err;
+        }   
+    }
+
+    getEmployees(){
+        try{
+            return this.connection.query(
+                `
+                SELECT 
+                    * 
+                FROM employee;
+                `
+            );
+        }
+        catch(err){
+            if(err) throw err;
+        }   
+    }
+
+    addRole(title, salary, departnemtId){
+        try{
+             this.connection.query(
+                'INSERT INTO role SET ?',
+                {
+                    title: `${title}`,
+                    salary: `${salary}`,
+                    department_id: `${departnemtId}`
+                },
+                function (err, res) {
+                    if (err) throw err;
+                    console.log(`\nSuccessfully added role with title:${title}, salary:${salary}, departmentId:${departnemtId}`);
+                    return res;
+                }
+            );
+        }
+        catch(err){
+            console.log("Error inserting role : "+ err );
+        }
+    }
+
+    addEmployee(firstName, lastName, roleId, managerId){
+        try{
+             this.connection.query(
+                'INSERT INTO employee SET ?',
+                {
+                    first_name: `${firstName}`,
+                    last_name: `${lastName}`,
+                    role_id: `${roleId}`,
+                    manager_id: `${managerId}`
+                },
+                function (err, res) {
+                    if (err) throw err;
+                    console.log(`\nSuccessfully added employee with firstName:${firstName}, lastName:${lastName}, roleId:${roleId}, managerId:{managerId}`);
+                    return res;
+                }
+            );
+        }
+        catch(err){
+            console.log("Error inserting role : "+ err );
+        }
     }
 
     addDepartment(deptName){
@@ -103,8 +181,6 @@ class DB{
         catch(err){
             console.log("Error inserting department : "+ err );
         }
-       
-
     }
 
     closeConnection(){
