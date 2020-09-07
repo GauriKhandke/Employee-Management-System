94e35a5cbe31826ca2c2c04885756a96798cb7bc
Gauri Khandke <khandkegauri@gmail.com>
2020-09-07 16:17:44 -0700

    Added query for view all employee's by manager and updated query for viewEmployeesbyDepartment

--- db/index.js
+++ db/index.js
@@ -29,7 +29,31 @@ class DB {
                 e1.id;
         `
 		);
-	}
+    }
+    
+    viewAllEmployeesByManager(){
+        return this.connection.query(
+            `
+            SELECT
+                e1.id AS ID,
+                e1.first_name AS First_Name,
+                e1.last_name AS Last_Name,
+                role.title AS Role,
+                department.name AS Department,
+                CONCAT(e2.first_name, ' ', e2.last_name) AS Manager,
+                role.salary AS Salary
+            FROM
+                employee e1
+            LEFT JOIN
+                role ON e1.role_id = role.id
+            LEFT JOIN
+                employee e2 ON e1.manager_id = e2.id
+		    LEFT JOIN department ON role.department_id = department.id
+		    ORDER BY
+                e1.manager_id;
+            `
+        );
+    }
 
 	viewAllRoles() {
 		return this.connection.query(
@@ -61,21 +85,25 @@ class DB {
 
 	viewEmployeesByDepartment() {
 		return this.connection.query(
-			`
-        SELECT 
-            employee.id AS id,
-            concat(first_name, " ", employee.last_name) AS Name,
-            role.title AS Title,
-            department.name AS Department,
-            role.salary AS Salary
-        FROM
-            ((employee
-        INNER JOIN role 
-            ON employee.role_id = role.id) 
-        INNER JOIN department 
-            ON role.department_id = department.id)
-        ORDER BY department.name;
-        `
+            `
+            SELECT
+                e1.id AS ID,
+                e1.first_name AS First_Name,
+                e1.last_name AS Last_Name,
+                role.title AS Role,
+                department.name AS Department,
+                CONCAT(e2.first_name, ' ', e2.last_name) AS Manager,
+                role.salary AS Salary
+            FROM
+                employee e1
+            LEFT JOIN
+                role ON e1.role_id = role.id
+            LEFT JOIN
+                employee e2 ON e1.manager_id = e2.id
+            LEFT JOIN department ON role.department_id = department.id
+            ORDER BY
+                department.name;
+            ` 
 		);
 	}
 
